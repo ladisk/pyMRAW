33c9a31367852bb7216888f35958814b42b7b023
Janko Slavič <janko.slavic@gmail.com>
2023-02-28 06:05:35 -0300

    update of setup

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ We developed this module while working on this publication:
 J. Javh, J. Slavič and M. Boltežar: The Subpixel Resolution of Optical-Flow-Based Modal Analysis,
 Mechanical Systems and Signal Processing, Vol. 88, p. 89–99, 2017
  
-If you find it useful, consider to cite us.
+Our recent research effort can be found here: http://lab.fs.uni-lj.si/ladisk/?what=incfl&flnm=research_filtered.php&keyword=optical%20methods
+
+If you find our research useful, consider to cite us.
 
 """
 
